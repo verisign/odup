51eecc927374cd1a08e90becab2c63326c4c9949
Casey Deccio <casey@deccio.net>
2016-02-29 16:30:14 -0500

    Include "private" domains from public suffix list

--- psl2odup.py
+++ psl2odup.py
@@ -39,11 +39,11 @@ import re
 import dns.name
 
 def import_names(f, i, p):
+    is_private = False
     with codecs.open(f, 'r', 'utf-8') as fh:
-        n = i
         for line in fh:
             if re.search('BEGIN PRIVATE', line) is not None:
-                n = p
+                is_private = True
 
             line = line.rstrip()
             line = re.sub(r'//.*', '', line)
@@ -53,30 +53,61 @@ def import_names(f, i, p):
             name = dns.name.from_text(line)
             tld = dns.name.from_text(name[-2])
 
-            if tld not in n:
-                n[tld] = set()
-            if tld != name:
-                n[tld].add(name)
+            if is_private:
+                public_suffix = longest_match(name, i)
+                if public_suffix is None:
+                    suffix = name
+                else:
+                    suffix = dns.name.Name(name[-(len(public_suffix)+1):])
+                if suffix not in p:
+                    p[suffix] = set()
+                p[suffix].add(name)
+            else:
+                if tld not in i:
+                    i[tld] = set()
+                i[tld].add(name)
 
-def export_zone(tld, names, zonefile_dir, server_names, conffile_fh):
+def longest_match(name, psl):
+    try:
+        suffixes = psl[dns.name.from_text(name[-2])]
+    except KeyError:
+        return None
+    while name != dns.name.root:
+        if name in suffixes:
+            return name
+        if dns.name.from_text('*', name.parent()) in suffixes and \
+                dns.name.from_text('!%s' % name[0], name.parent()) not in suffixes:
+            return name
+        if dns.name.from_text('*', name) in suffixes:
+            return name
+        name = name.parent()
+    return None
 
-    filename = os.path.join(zonefile_dir, 'db._odup.%s' % (tld.to_text().rstrip('.')))
-    conffile_fh.write('zone "_odup.%s" {\n' % tld.to_text())
+def export_zone(suffix, names, zonefile_dir, server_names, conffile_fh):
+    filename = os.path.join(zonefile_dir, 'db._odup.%s' % (suffix.to_text().rstrip('.')))
+    conffile_fh.write('zone "_odup.%s" {\n' % suffix.to_text())
     conffile_fh.write('\ttype master;\n')
     conffile_fh.write('\tfile "%s";\n' % filename)
     conffile_fh.write('\tallow-transfer { any; };\n')
     conffile_fh.write('};\n')
 
+    if len(suffix) == 2:
+        fetch_str = ' +fetch:axfr://'
+    else:
+        fetch_str = ''
+
     with open(filename, 'w+') as fh:
-        fh.write('$ORIGIN _odup.%s\n' % (tld.to_text()))
+        fh.write('$ORIGIN _odup.%s\n' % (suffix.to_text()))
         fh.write('$TTL 604800\n')
-        fh.write('@\tSOA\t%s root.nic.%s 1 1800 900 604800 86400\n' % (server_names[0].to_text(), tld.to_text()))
+        fh.write('@\tSOA\t%s root.nic.%s 1 1800 900 604800 86400\n' % (server_names[0].to_text(), suffix.to_text()))
         for server_name in server_names:
             fh.write('\tNS\t%s\n' % (server_name.to_text()))
-        fh.write('\tTXT\t"v=odup1 +bound +fetch:axfr:// -all"\n')
+        fh.write('\tTXT\t"v=odup1 +bound%s -all"\n' % fetch_str)
 
         for name in names:
-            name = name.relativize(tld)
+            name = name.relativize(suffix)
+            if name == dns.name.empty:
+                continue
             if name[0][0].startswith('!'):
                 fh.write('%s\tTXT\t"v=odup1 +org"\n' % (name.to_text().lstrip('!')))
             elif name[0] == '*':
@@ -110,12 +141,12 @@ def main():
     import_names(public_suffix_file, icann_names, private_names)
 
     with open(named_conf_inc_filename, 'w+') as fh:
-        for tld in icann_names:
+        for suffix, names in icann_names.items() + private_names.items():
             if server_name is not None:
                 server_names = (server_name,)
             else:
-                server_names = (dns.name.Name(('a', 'odup-servers', tld[-2])),)
-            export_zone(tld, icann_names[tld], zonefile_dir, server_names, fh)
+                server_names = (dns.name.Name(('a', 'odup-servers') + name),)
+            export_zone(suffix, names, zonefile_dir, server_names, fh)
 
 if __name__ == '__main__':
     main()
